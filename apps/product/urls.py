8c34b5c2ee5d5d034ac1ac2db37beef22fbca611
BekhanRay <bekhanrayamjanov@gmail.com>
2024-02-08 10:55:40 +0600

    SSL version

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('product/<int:pk>/', ProductDetailView.as_view()),
     path('category/', CategoryListView.as_view(), name='category'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('cart/update/', update_cart_item, name='update_cart'),
-    path('cart/add/', add_to_cart, name='add_to_cart'),
-    path('cart/remove/', remove_from_cart, name='remove_from_cart'),
+    path('cart/update/<int:pk>', update_cart_item, name='update_cart'),
+    path('cart/add/<int:pk>', add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:pk>', remove_from_cart, name='remove_from_cart'),
     path('type/', TypeListView.as_view(), name='type'),
     ] + static(base.MEDIA_URL, document_root=base.MEDIA_ROOT)
